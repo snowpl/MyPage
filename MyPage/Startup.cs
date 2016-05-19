bb53f970543beb6de0e5faa8474f1912156e0bde
Kacper Werema <kacper.werema@gmail.com>
2016-05-19 17:40:09 +0200

    no message

---
+++ MyPage/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyPage.Startup))]
+namespace MyPage
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
